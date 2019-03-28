d1ee3c83355c9b7e0e7bb073f77f24184dac9f13
Marc Marino <marc.a.marino@gmail.com>
2019-03-28 11:05:26 -0400

    worked through props, state, and style

---
+++ styleSheets.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  green: {
+    textAlign: 'center',
+    color: 'green',
+    backgroundColor: 'grey',
+  }
+  });
+
+
+export default styles;
